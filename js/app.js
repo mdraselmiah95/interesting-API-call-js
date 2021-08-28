30a32c275a5716a13ad46c59a61dd0c70cf79ea6
rasel <russelroy15@gmail.com>
2021-08-29 01:21:43 +0600

    new style added

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const setComment = (comments) => {
             <div class = "border border-1 rounded bg-light py-3 px-2 w-75 mx-auto mb-2">
                 <h2 class = "fw-bold text-success">${comment.id}</h2>
                 <h3 class = "text-primary my-3">${comment.title}</h3> 
-                <p>${comment.body}</p> 
+                <p class = "text-warning"> ${comment.body}</p> 
             </div>
 
         `;
